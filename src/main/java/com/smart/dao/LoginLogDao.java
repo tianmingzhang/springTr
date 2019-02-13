33483dd39a7feef3eb27bf1d806abaf0b16b308f
tianmingzhang <38976941+tianmingzhang@users.noreply.github.com>
2019-02-13 21:08:56 +0800

    commit by ztm 2/13

--- src/main/java/com/smart/dao/LoginLogDao.java
+++ src/main/java/com/smart/dao/LoginLogDao.java
@@ -6,15 +6,20 @@ import org.springframework.stereotype.Repository;
 
 import com.smart.domain.LoginLog;
 
+import java.util.Random;
+
 @Repository
 public class LoginLogDao {
 	private JdbcTemplate jdbcTemplate;
 
 	//保存登陆日志SQL
-	private final static String INSERT_LOGIN_LOG_SQL= "INSERT INTO t_login_log(user_id,ip,login_datetime) VALUES(?,?,?)";
+	private final static String INSERT_LOGIN_LOG_SQL= "INSERT INTO t_login_log(login_log_id,user_id,ip,login_datetime) VALUES(?,?,?,?)";
 	
 	public void insertLoginLog(LoginLog loginLog) {
-		Object[] args = { loginLog.getUserId(), loginLog.getIp(),
+		Random rand = new Random();
+		int log_id;
+		log_id = rand.nextInt(10000);
+		Object[] args = { log_id,loginLog.getUserId(), loginLog.getIp(),
 				          loginLog.getLoginDate() };
 		jdbcTemplate.update(INSERT_LOGIN_LOG_SQL, args);
 	}
